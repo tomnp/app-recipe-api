38715b148d305d121f0556775632384e59098961
Pele Jodan <pelejodan@MacBookPro.lan>
2025-12-29 22:09:56 +0700

    Add postgresql adapter

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Docstring for app.app.calc
+"""
+
+def add(x, y):
+  """Add x and y and return the results."""
+  return x + y
+
+def subtract(x, y):
+  """Subtract y from x and return the results."""
+  return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+  """Tests for calc module."""
+
+  def test_add_numbers(self):
+    """Test adding two numbers to gethers."""
+    res = calc.add(5,6)
+
+    self.assertEqual(res, 11)
+
+  def test_substract_numbers(self):
+    """Test subtracting two numbers to gethers."""
+    res = calc.subtract(10,15)
+
+    self.assertEqual(res, 5)
